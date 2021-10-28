b9d4dd6bf46b19b3e8238a8ac94bdf3c8d5dced6
Gustavo Zorzeto <89036888+gzorzeto@users.noreply.github.com>
2021-10-28 17:15:57 -0300

    Update GA_Ball.py

--- GA_Ball.py
+++ GA_Ball.py
@@ -1,17 +1,21 @@
 from math import sqrt
 from random import randint, choices, random, randrange
+from pprint import pprint
+import matplotlib.pyplot as plt
 
 #==========================================================================================================================================================================
 
-d_vetor = 2 # Cada vetor poderá ter um valor entre (- d_vetor e + d_vetor)
+d_vetor = 1 # Cada vetor poderá ter um valor entre (- d_vetor e + d_vetor) em x e (0 e +d_vetor em y)
 n_genes = 250 # Número de pares de vetores (x,y) e consequentemente pares de posições da bola
-n_bolas = 100 # Cada bola recebe n vetores (n_genes) que vão ditar sua movimentação, formando n_genes de posições (genes)
-vel = 25 # Velocidade aplicada a cada vetor para movimentar cada bola
-xi_bola = 1 # Posição inicial da bola em x
-yi_bola = 1 # Posição inicial da bola em y
+n_bolas = 200 # Cada bola recebe n vetores (n_genes) que vão ditar sua movimentação, formando n_genes de posições (genes)
+vel = 10 # Velocidade aplicada a cada vetor para movimentar cada bola
+xi_bola = 0 # Posição inicial da bola em x
+yi_bola = 0 # Posição inicial da bola em y
 bolas = []
-x_obj = 50 # Posição final desejada em x
-y_obj = 50 # Posição final desejada em y
+x_obj = 250 # Posição final desejada em x
+y_obj = 250 # Posição final desejada em y
+dist_max = sqrt(x_obj**2 + y_obj**2)
+mut_prob = 0.20
 
 # É importante ressaltar que cada bola corresponde a um conjunto de posições (x,y) que descrevem o caminho que essa bola percorreu!
 #==========================================================================================================================================================================
@@ -22,73 +26,63 @@ y_obj = 50 # Posição final desejada em y
 def gerar_genoma (xi_bola, yi_bola, n_genes):
     vetores = [(randint(-d_vetor,d_vetor), randint(-d_vetor, d_vetor)) for _ in range (n_genes)]
     genoma = [] # Lista com posições da bola no eixo cartesiano
-    for i in vetores:
+    for vetor in vetores:
         genoma.append((xi_bola, yi_bola))
-        xi_bola += vel*i[0]
-        yi_bola += vel*i[1]
+        xi_bola += vel*vetor[0]
+        yi_bola += vel*vetor[1]
     return genoma
 
+#pprint(gerar_genoma(xi_bola, yi_bola, n_genes)[-1])
 #==========================================================================================================================================================================
 
 # Gerar população de bolas aleatórias
-def gerar_populacao (n_bolas, n_genes):
+def gerar_populacao (n_bolas):
     return [gerar_genoma (xi_bola, yi_bola, n_genes) for _ in range (n_bolas)] 
-#print(gerar_populacao(n_bolas, n_genes))
 
+#pprint(gerar_populacao(n_bolas))
 #==========================================================================================================================================================================
 
-#fitness média para todas as posições (x,y) de uma bola (genoma)
-def fitness (genoma): 
-    # Calcular fitness para cada posição (x,y) da bola
-    def individual_fitness (x, y):
-        global x_obj, y_obj
-        d = sqrt((x_obj - x)**2+(y_obj - y)**2)
-        if d == 0:
-            return 1000
-        else:
-            return 1/d
-    # Calcular a fitness média para a bola (genoma)
-    acum_fitness = 0
-    for vetor in genoma:
-        acum_fitness += individual_fitness(vetor[0], vetor[1])
-    avr_fitness = acum_fitness/len(genoma)
-    return avr_fitness
-
+def fitness (genoma):
+    global x_obj, y_obj
+    x, y = genoma[-1][0], genoma[-1][1]
+    d = sqrt((x_obj - x)**2+(y_obj - y)**2)
+    d_normalisada = d/dist_max
+    return max(0, 1 - d_normalisada)
 
 #==========================================================================================================================================================================
 
 def selecionar_par (populacao):
     return choices(
         population = populacao,
-        weights = [2**(fitness(genoma)*100) for genoma in populacao],
+        weights = [fitness(genoma)*100 for genoma in populacao],
         k = 2)
-        
+
+#pprint(selecionar_par(gerar_populacao(n_bolas)))        
 #==========================================================================================================================================================================
 
 def crossover (a, b):
     p = randint(1, n_genes - 1)
-    # Retorna uma tupla de "filhos"
+    # Retorna uma tupla de "filhos" por meio de um single point crossover
     return a[0:p] + b[p:], b[0:p] + a[p:]
 
+#pais = selecionar_par(gerar_populacao(n_bolas))
+#pprint(crossover(pais[0],pais[1]))
 #==========================================================================================================================================================================
 
-def mutacao (genoma, probabilidade = 0.02):
-    index = randrange(1, len(genoma)) # Nunca mutar o primeiro gene (primeira posição da bola)
-    if random() > probabilidade:
-        genoma[index] = genoma[index]
+def mutacao (genoma, mut_prob):
+    if random() > mut_prob:
+        genoma = genoma
     else:
-        vetor_mut = (randint(-d_vetor,d_vetor), randint(-d_vetor, d_vetor))
-        # Aplicar vetor mutado na posição anterior da bola em x e y
-        x_bola_mut = genoma[index - 1][0] + vel*vetor_mut[0]
-        y_bola_mut = genoma[index - 1][1] + vel*vetor_mut[1]
-        genoma[index] = (x_bola_mut, y_bola_mut)
+        genoma = gerar_genoma(xi_bola,yi_bola, n_genes)
     return genoma
 
 #==========================================================================================================================================================================
 
 def run_evolucao (limite_fitness, limite_geracoes):
-    # Gerar população inicial
-    populacao = gerar_populacao(n_bolas, n_genes)
+    # Gerar população inicial e criar lista para armazenar todas as gerações
+    populacao = gerar_populacao(n_bolas)
+    geracoes = []
+    fitness_values = []
     # Entrar no loop para criar n_gerações
     for i in range (limite_geracoes):
         populacao = sorted(populacao, key = lambda genoma: fitness(genoma), reverse = True)
@@ -103,16 +97,18 @@ def run_evolucao (limite_fitness, limite_geracoes):
         for _ in range(int(len(populacao)/2)-1):
             pais = selecionar_par(populacao)
             filho_a, filho_b = crossover(pais[0], pais[1])
-            filho_a = mutacao (filho_a)
-            filho_b = mutacao (filho_b)
-            prox_geracao += [filho_a, filho_b]
-        print(f"Geração: {i}")
-        print(f"Fitness:{fitness(populacao[0])}")
-        print("=====================================================================================================================")
+            filho_a = mutacao (filho_a, mut_prob)
+            filho_b = mutacao (filho_b, mut_prob)
+            prox_geracao += [filho_a, filho_b]    
         populacao = prox_geracao
-    populacao = sorted(populacao, key = lambda genoma: fitness(genoma), reverse = True)
+        populacao = sorted(populacao, key = lambda genoma: fitness(genoma), reverse = True)
+        print(f"Geração: {i}")
+        geracoes.append(i)
+        fitness_values.append(fitness(populacao[0]))
+    return geracoes, fitness_values
 
-    return populacao, i
-
-run_evolucao(limite_fitness = 1, limite_geracoes = 100)
-            
+geracoes, fitness_values = run_evolucao(limite_fitness = 1, limite_geracoes = 1000)
+plt.plot(geracoes, fitness_values)
+plt.xlabel('Geração')
+plt.ylabel("Valor Fitness")
+plt.show()
